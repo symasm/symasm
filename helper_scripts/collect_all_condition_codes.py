9291f23f81a89d3f0aa61d64b7d86127e7ebfc44
Alexander Tretyak <alextretyak@users.noreply.github.com>
2024-04-26 13:53:38 +1000

    Still, the negative comparison condition codes make sense

--- helper_scripts/collect_all_condition_codes.py
+++ helper_scripts/collect_all_condition_codes.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 from collections import OrderedDict
 
 sources = [('https://www.felixcloutier.com/x86/jcc', '', 'rel8'),
-           #('https://www.felixcloutier.com/x86/cmovcc', 'REX.W + ', ''),
-           #('https://www.felixcloutier.com/x86/setcc', 'REX + ', ''),
+           ('https://www.felixcloutier.com/x86/cmovcc', 'REX.W + ', ''),
+           ('https://www.felixcloutier.com/x86/setcc', 'REX + ', ''),
           ]
 
 unambiguous_cc = set()
@@ -30,7 +30,7 @@ for source, startswith, endswith in sources:
         if cc[0] == 'N' and cc not in ('NP', 'NC'):
             for i in instructions:
                 if i[0] == opcode and i[1][len(commonprefix)] != 'N':
-                    found = i[1].split(' ')[0][len(commonprefix):] # there is an instruction with the same opcode, but without N
+                    #found = i[1].split(' ')[0][len(commonprefix):] # there is an instruction with the same opcode, but without N
                     break
         elif cc == 'PE':
             found = 'P'
@@ -46,8 +46,13 @@ for source, startswith, endswith in sources:
 cc_to_sym = OrderedDict()
 sym_used = set()
 for cc in sorted(unambiguous_cc):
+    orig_cc = cc
+    sym = ''
+    if cc[0] == 'N' and cc[1] in 'ABGL':
+        sym = '!'
+        cc = cc[1:]
     if cc[0] in 'ABGL':
-        sym = 'u' * (cc[0] in 'AB') + ('>' if cc[0] in 'AG' else '<')
+        sym += 'u' * (cc[0] in 'AB') + ('>' if cc[0] in 'AG' else '<')
         if cc[1:] == 'E':
             sym += '='
         else:
@@ -57,7 +62,7 @@ for cc in sorted(unambiguous_cc):
     elif cc == 'NE':
         sym = '!='
     elif cc.endswith('CXZ'):
-        sym = cc[:-1] + ' == 0'
+        sym = cc[:-1] + '_==_0'
     elif cc[-1] in 'OCZSP':
         if cc[0] == 'N':
             assert(len(cc) == 2)
@@ -71,14 +76,14 @@ for cc in sorted(unambiguous_cc):
     assert sym not in sym_used, sym
     sym_used.add(sym)
 
-    cc_to_sym[cc] = sym
+    cc_to_sym[orig_cc] = sym
 
 acc_to_sym = OrderedDict()
 for cc, dcc in sorted(ambiguous_cc):
     acc_to_sym[cc] = cc_to_sym[dcc]
 
-print(textwrap.fill('cc_to_sym = ' + repr(cc_to_sym)[12:-1].lower()))
+print(textwrap.fill('cc_to_sym = ' + repr(dict(cc_to_sym)).lower()).replace('_==_', ' == '))
 print()
-print(textwrap.fill('cc_to_sym.update' + repr(acc_to_sym)[11:].lower()))
+print(textwrap.fill('cc_to_sym.update(' + repr(dict(acc_to_sym)).lower()) + ')')
 
 # >[https://www.felixcloutier.com/x86/setcc]:‘Appendix B, “EFLAGS Condition Codes,” in the Intel® 64 and IA-32 Architectures Software Developer’s Manual, Volume 1, shows the alternate mnemonics for various test conditions.’
